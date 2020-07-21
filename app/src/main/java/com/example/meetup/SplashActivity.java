ab385aa6a0ba2a8448064f2bc65d2a81e88991fc
Loc Trinh <42801211+loctrinh97@users.noreply.github.com>
2020-07-21 17:49:20 +0700

    design login & sign up

---
+++ app/src/main/java/com/example/meetup/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.example.meetup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class SplashActivity extends AppCompatActivity {
+private Handler delay = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // hide notification bar
+        requestWindowFeature(Window.FEATURE_ACTION_MODE_OVERLAY);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        // delay & go to home screen
+        delay.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Intent intent = new Intent(SplashActivity.this, SignupLoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        },2000);
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        delay.removeCallbacksAndMessages(null);
+    }
+}
