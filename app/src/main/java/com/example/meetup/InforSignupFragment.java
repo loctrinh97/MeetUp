9e2a29370c8054e9c5c3baee589606f134382ee2
Loc Trinh <42801211+loctrinh97@users.noreply.github.com>
2020-07-22 17:37:13 +0700

    custom lại view

--- app/src/main/java/com/example/meetup/InforSignupFragment.java
+++ app/src/main/java/com/example/meetup/InforSignupFragment.java
@@ -7,8 +7,8 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-public class SignupFragment extends Fragment {
-    public SignupFragment() {
+public class InforSignupFragment extends Fragment {
+    public InforSignupFragment() {
         // Required empty public constructor
     }
 
@@ -17,6 +17,6 @@ public class SignupFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        return inflater.inflate(R.layout.fragment_signup_infor, container, false);
     }
 }
--- app/src/main/java/com/example/meetup/LoginFragment.java
+++ app/src/main/java/com/example/meetup/LoginFragment.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class LoginFragment extends Fragment {
+    Button btnSignUp;
+    Button btnLogin;
+    FragmentManager fragmentManager;
+    ImageView imgSignUp;
+    ImageView imgLogin;
     public LoginFragment() {
         // Required empty public constructor
     }
@@ -17,6 +27,48 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        final View view = inflater.inflate(R.layout.fragment_login, container, false);
+        btnLogin = view.findViewById(R.id.btnLogin);
+        btnSignUp = view.findViewById(R.id.btnSignUp);
+        imgSignUp = view.findViewById(R.id.imgSignUp);
+        imgLogin = view.findViewById(R.id.imgLogin);
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragmentSignupInfor();
+                imgSignUp.setVisibility(View.VISIBLE);
+                imgLogin.setVisibility(View.GONE);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragmentLoginInfor();
+                imgSignUp.setVisibility(View.GONE);
+                imgLogin.setVisibility(View.VISIBLE);
+            }
+        });
+        return view;
+    }
+
+    private void addFragmentSignupInfor() {
+        fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container2, new InforSignupFragment(), null);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
+
+    private void addFragmentLoginInfor() {
+        fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container2, new InforLoginFragment(), null);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 }
