d011c8195bf1e39f0f9d65878cb0c8a99f86899a
Loc Trinh <42801211+loctrinh97@users.noreply.github.com>
2020-07-30 10:59:00 +0700

    fix conflict 2

---
+++ app/src/main/java/com/example/meetup/UI/Login/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.example.meetup.UI.Login;
+
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.meetup.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        addFragmentLogin();
+        addFragmentSignupInfor();
+        Log.d("test", "activity: onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("test", "activity: onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void addFragmentLogin() {
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container1, new LoginFragment(), null);
+        fragmentTransaction.commit();
+    }
+
+    private void addFragmentSignupInfor() {
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container2, new InforSignupFragment(), null);
+        fragmentTransaction.commit();
+
+    }
+}
---
+++ app/src/main/java/com/example/meetup/services/UserService.java
@@ -0,0 +1,21 @@
+package com.example.meetup.services;
+
+import com.example.meetup.Model.User;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface UserService {
+@POST("register")
+Call<User> signUp(@Body User post);
+    @FormUrlEncoded
+    @POST("register")
+    Call<User> signUp(
+            @Field("name") String name,
+            @Field("email") String email,
+            @Field("password") String password
+    );
+}
